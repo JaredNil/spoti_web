b2d56e716a8dba14265b225b6271a3fddf43f8f2
JaredN <77531492+JaredNil@users.noreply.github.com>
2025-08-28 12:05:41 +0800

    import-fix

--- src/app/playlist/liked/page.tsx
+++ src/app/playlist/liked/page.tsx
@@ -1,11 +1,11 @@
 import { PlaylistTitle } from '../ui/playlistTitle'
 import { TrackViewVender } from '../ui/trackView/trackView'
 
-import { fetchAlbum } from '@/entities/album'
+import { fetchAlbumServer } from '@/entities/album'
 import { fetchTrackesServer } from '@/entities/track'
 
 export default async function PlaylistPageLiked() {
-	const album = await fetchAlbum(1)
+	const album = await fetchAlbumServer(1)
 	const trackes = await fetchTrackesServer(album.trackesId as number[])
 
 	return (
