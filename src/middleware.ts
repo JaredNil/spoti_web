5e4404480ac648c53c1cb5f50b4b2f0b2be5af59
JaredN <77531492+JaredNil@users.noreply.github.com>
2025-09-01 23:26:25 +0800

    middleware-condition-auth-pages-access

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,14 +2,19 @@ import { NextResponse } from 'next/server'
 
 import { auth } from './auth'
 
+const authRoutes = ['/auth/login', '/auth/register', '/auth']
+
 export default auth((req) => {
-	if (!req.auth) {
+	if (req.auth && authRoutes.includes(req.nextUrl.pathname)) {
+		return NextResponse.redirect(new URL('/home', req.url))
+	}
+	if (!req.auth && !authRoutes.includes(req.nextUrl.pathname)) {
 		return NextResponse.redirect(new URL('/auth/login', req.url))
 	}
 })
 
 export const config = {
 	matcher: [
-		'/((?!api|_next/static|_next/image|_next/image*|favicon.ico|content/.*|favicon/.*|homepage/.*|auth/.*|home/?$).*)',
+		'/((?!api|_next/static|_next/image|_next/image*|favicon.ico|content/.*|favicon/.*|homepage/.*|home/?$).*)',
 	],
 }
