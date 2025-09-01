71752d710153cff064081e259229d8201a7f043f
JaredN <77531492+JaredNil@users.noreply.github.com>
2025-09-01 22:19:28 +0800

    auth-v5-migrate

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,58 +1,73 @@
-import { jwtVerify } from 'jose'
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-import { getToken } from 'next-auth/jwt'
 
-const publicRoutes = ['/auth/login', '/auth/register', '/auth', '/', '/home']
-const authRoutes = ['/auth/login', '/auth/register', '/auth']
+import { auth } from './auth'
 
-const secret = process.env.AUTH_SECRET
-
-export async function middleware(req: NextRequest) {
-	const path = req.nextUrl.pathname
-	const isPublicRoute = publicRoutes.includes(path)
-	const isPublicContent = req.nextUrl.pathname.startsWith('/content')
-
-	const isAuthRoute = authRoutes.includes(path)
-
-	const token = await getToken({ req, secret })
-
-	console.log('TOKEN', token)
-
-	const sessionCookie = req.cookies
-		.get('__Secure-authjs.session-token')
-		?.value?.trim()
-
-	let token2: any = null
-	if (sessionCookie && sessionCookie.split('.').length === 3) {
-		try {
-			const { payload } = await jwtVerify(
-				sessionCookie,
-				new TextEncoder().encode(secret)
-			)
-			token2 = payload
-		} catch (err) {
-			// JWT повреждён или просрочен
-			console.warn('JWT verify error:', err)
-		}
+export default auth((req) => {
+	if (!req.auth) {
+		return NextResponse.redirect(new URL('/auth/login', req.url))
 	}
-	console.log('TOKEN2:', sessionCookie)
+})
 
-	if (!isPublicRoute && !token && !isPublicContent) {
-		console.log('START LOG')
-		console.log('isPublicRoute', isPublicRoute)
-		console.log('isPublicContent', isPublicContent)
-		console.warn('REDIRECT REQUEST WITHOUT AUTH', path)
-		return NextResponse.redirect(new URL('/auth/login', req.nextUrl))
-	}
-	if (isAuthRoute && token) {
-		// redirect from auth pages to content
-		console.warn('REDIRECT REQUEST FROM AUTH TO CONTENT', path)
-		return NextResponse.redirect(new URL('/home', req.nextUrl))
-	}
-
-	return NextResponse.next()
-}
+// Optionally, don't invoke Middleware on some paths
 export const config = {
-	matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+	matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
+
+// import { jwtVerify } from 'jose'
+// import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
+// import { getToken } from 'next-auth/jwt'
+
+// const publicRoutes = ['/auth/login', '/auth/register', '/auth', '/', '/home']
+// const authRoutes = ['/auth/login', '/auth/register', '/auth']
+
+// const secret = process.env.AUTH_SECRET
+
+// export async function middleware(req: NextRequest) {
+// 	const path = req.nextUrl.pathname
+// 	const isPublicRoute = publicRoutes.includes(path)
+// 	const isPublicContent = req.nextUrl.pathname.startsWith('/content')
+
+// 	const isAuthRoute = authRoutes.includes(path)
+
+// 	const token = await getToken({ req, secret })
+
+// 	console.log('TOKEN', token)
+
+// 	const sessionCookie = req.cookies
+// 		.get('__Secure-authjs.session-token')
+// 		?.value?.trim()
+
+// 	let token2: any = null
+// 	if (sessionCookie && sessionCookie.split('.').length === 3) {
+// 		try {
+// 			const { payload } = await jwtVerify(
+// 				sessionCookie,
+// 				new TextEncoder().encode(secret)
+// 			)
+// 			token2 = payload
+// 		} catch (err) {
+// 			// JWT повреждён или просрочен
+// 			console.warn('JWT verify error:', err)
+// 		}
+// 	}
+// 	console.log('TOKEN2:', sessionCookie)
+
+// 	if (!isPublicRoute && !token && !isPublicContent) {
+// 		console.log('START LOG')
+// 		console.log('isPublicRoute', isPublicRoute)
+// 		console.log('isPublicContent', isPublicContent)
+// 		console.warn('REDIRECT REQUEST WITHOUT AUTH', path)
+// 		return NextResponse.redirect(new URL('/auth/login', req.nextUrl))
+// 	}
+// 	if (isAuthRoute && token) {
+// 		// redirect from auth pages to content
+// 		console.warn('REDIRECT REQUEST FROM AUTH TO CONTENT', path)
+// 		return NextResponse.redirect(new URL('/home', req.nextUrl))
+// 	}
+
+// 	return NextResponse.next()
+// }
+// export const config = {
+// 	matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+// }
