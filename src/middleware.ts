f88ca61063097a3ab60e6d06f81cf0eb683269c0
JaredN <77531492+JaredNil@users.noreply.github.com>
2025-09-01 21:02:35 +0800

    jose-cookie-read

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,42 @@
+import { jwtVerify } from 'jose'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { getToken } from 'next-auth/jwt'
 
 const publicRoutes = ['/auth/login', '/auth/register', '/auth', '/', '/home']
+const authRoutes = ['/auth/login', '/auth/register', '/auth']
+
+const secret = process.env.AUTH_SECRET
 
 export async function middleware(req: NextRequest) {
 	const path = req.nextUrl.pathname
 	const isPublicRoute = publicRoutes.includes(path)
 	const isPublicContent = req.nextUrl.pathname.startsWith('/content')
 
-	const token = await getToken({ req, secret: process.env.AUTH_SECRET })
+	const isAuthRoute = authRoutes.includes(path)
+
+	const token = await getToken({ req, secret })
+
+	console.log('TOKEN', token)
+
+	const sessionCookie = req.cookies.get('next-auth.session-token')?.value
+	const secret2 = new TextEncoder().encode(process.env.AUTH_SECRET!)
+	const { payload } = await jwtVerify(sessionCookie ?? '', secret2)
+	const token2 = payload
+
+	console.log('TOKEN2', token2)
 
 	if (!isPublicRoute && !token && !isPublicContent) {
 		console.log('START LOG')
-		console.log(token)
-		console.log(isPublicRoute)
-		console.log(isPublicContent)
+		console.log('isPublicRoute', isPublicRoute)
+		console.log('isPublicContent', isPublicContent)
 		console.warn('REDIRECT REQUEST WITHOUT AUTH', path)
 		return NextResponse.redirect(new URL('/auth/login', req.nextUrl))
+	}
+	if (isAuthRoute && token) {
+		// redirect from auth pages to content
+		console.warn('REDIRECT REQUEST FROM AUTH TO CONTENT', path)
+		return NextResponse.redirect(new URL('/home', req.nextUrl))
 	}
 
 	return NextResponse.next()
